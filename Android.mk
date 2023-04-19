username0136/dump_device_xiaomi_sm8250-common

sm8250-common: Add munch to the list of supported devices
Change-Id: Ie500a81c711a157f6707f8f25c361685072762fd

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollon cas cmi elish enuma lmi thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth apollon cas cmi elish enuma lmi munch thyme umi,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 

setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "alioth apollon cas cmi elish enuma lmi thyme umi"
+write_headers "alioth apollon cas cmi elish enuma lmi munch thyme umi"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true

PixelExtended-Devices/munch_device_xiaomi_sm8250-common

Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollo cas cmi elish enuma lmi thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth apollo cas cmi elish enuma lmi munch thyme umi,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 

setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "alioth apollo cas cmi elish enuma lmi thyme umi"
+write_headers "alioth apollo cas cmi elish enuma lmi munch thyme umi"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true